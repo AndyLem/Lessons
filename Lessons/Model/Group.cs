bb5a8de1a16613df1a0f07f1e23cd120b9273361
Andrei Lemzikov <andylem@gmail.com>
2012-06-11 16:55:24 +0300

    Base Model classes added

---
+++ Lessons/Model/Group.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lessons.Model
+{
+    public class Group : IID
+    {
+        public string ID
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
