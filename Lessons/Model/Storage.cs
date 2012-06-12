fdbc214b709fb7e8625be03e7e2fde18cf46415a
Andrei Lemzikov <andylem@gmail.com>
2012-06-12 10:01:03 +0300

    Storage implemented as a singleton

--- Lessons/Model/Storage.cs
+++ Lessons/Model/Storage.cs
@@ -5,7 +5,26 @@ using System.Text;
 
 namespace Lessons.Model
 {
+    /// <summary>
+    /// Classic Singleton implementation of the Storage.Data instance
+    /// </summary>
     public class Storage
     {
+        protected static Storage _data;
+        
+        public static Storage Data
+        {
+            get
+            {
+                if (_data == null)
+                    _data = new Storage();
+                return _data;
+            }
+        }
+
+        protected Storage()
+        {
+
+        }       
     }
 }
